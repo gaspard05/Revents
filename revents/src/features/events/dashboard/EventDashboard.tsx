ad761150c42d90833081ea2e07879e723e3923ee
gaspard <gnzamura@hotmail.com>
2026-05-01 21:06:47 +0100

    added animation with motion package

--- revents/src/features/events/dashboard/EventDashboard.tsx
+++ revents/src/features/events/dashboard/EventDashboard.tsx
@@ -3,6 +3,7 @@ import { events } from "../../../lib/data/sampleData";
 import EventCard from "./EventCard";
 import EventForm from "./form/EventForm";
 import type { AppEvent } from "../../../lib/types";
+import { AnimatePresence, motion } from "motion/react";
 
 type Props = {
   formOpen: boolean;
@@ -23,13 +24,35 @@ export default function EventDashboard({ formOpen, setFormOpen }: Props) {
 
   return (
     <div className="flex flex-row w-full gap-6">
-      <div className="w-3/5 flex flex-col gap-4">
-        {appEvents.map((event) => (
-          <EventCard key={event.id} event={event} />
-        ))}
+      <div className="w-3/5">
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -200 }}
+            transition={{ duration: 0.3, type: "tween", ease: "easeInOut" }}
+          >
+            <div className="flex flex-col gap-4">
+              {appEvents.map((event) => (
+                <EventCard key={event.id} event={event} />
+              ))}
+            </div>
+          </motion.div>
+        </AnimatePresence>
       </div>
       <div className="w-2/5">
-        {formOpen && <EventForm setFormOpen={setFormOpen} />}
+        <AnimatePresence>
+          {formOpen && (
+            <motion.div
+              initial={{ opacity: 0, x: 200 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: 200 }}
+              transition={{ duration: 0.3, type: "tween", ease: "easeInOut" }}
+            >
+              <EventForm setFormOpen={setFormOpen} />
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
